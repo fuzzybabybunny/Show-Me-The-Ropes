14448754b7e26e73e43bf22139328d51a00a25eb
Screen Goose <sarrieschaz@hotmail.com>
2014-04-08 09:26:14 +0800

    Moved JS into new file

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,42 @@
+function initialize() {
+      var map_canvas = document.getElementById('map_canvas');
+      var myLatlng = new google.maps.LatLng(22.25, 114.1667);
+      var map_options = {
+        center: myLatlng,
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [
+            {
+              stylers: [
+                { hue: "#00ffe6" },
+                { saturation: -20 }
+              ]
+            },{
+              featureType: "road",
+              elementType: "geometry",
+              stylers: [
+                { lightness: 100 },
+                { visibility: "simplified" }
+              ]
+            },{
+              featureType: "road",
+              elementType: "labels",
+              stylers: [
+                { visibility: "off" }
+              ]
+            }
+          ]
+
+       }
+      var map = new google.maps.Map(map_canvas, map_options)
+
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title:"Hello World!"
+      });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    console.log("yoyo")
