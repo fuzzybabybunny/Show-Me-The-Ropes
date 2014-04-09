46a39b374520b322eb56419b42b9b16b3b43e5e9
Screen Goose <sarrieschaz@hotmail.com>
2014-04-09 09:03:53 +0800

    Added pin_drop.js

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -47,19 +47,21 @@ function initialize() {
 
 
 // ADDING MARKERS
-  function placeMarker(location) {
-  var marker = new google.maps.Marker({
-      position: location,
-      map: map
-  });
 
-  map.setCenter(location);
-  console.log(location);
-  }
 
-  google.maps.event.addListener(map, 'click', function(event) {
-    placeMarker(event.latLng);
-  });
+  // function placeMarker(location) {
+  // var marker = new google.maps.Marker({
+  //     position: location,
+  //     map: map
+  // });
+
+  // map.setCenter(location);
+  // console.log(location);
+  // }
+
+  // google.maps.event.addListener(map, 'click', function(event) {
+  //   placeMarker(event.latLng);
+  // });
 
 
 // LISTENING FOR ICON CLICKS
@@ -71,3 +73,4 @@ function initialize() {
 
 // CREATING THE MAP
 google.maps.event.addDomListener(window, 'load', initialize);
+
---
+++ app/assets/javascripts/pin_drop.js
@@ -0,0 +1,16 @@
+$( "#addpin" ).on( "click", function() {
+  console.log("yoyo");
+  // function placeMarker(location) {
+  // var marker = new google.maps.Marker({
+  //     position: location,
+  //     map: map
+  // });
+
+  // map.setCenter(location);
+  // console.log(location);
+  // }
+
+  // google.maps.event.addListener(map, 'click', function(event) {
+  //   placeMarker(event.latLng);
+  // });
+  });
