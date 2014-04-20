96812075730542cc64cc8419b13280573e5c4e71
Your NameMuWahLee <mlee2569@hotmail.com>
2014-04-20 15:49:16 +0800

    add CRUD methods to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
 
+  before_action :is_authenticated?
+  before_action :get_user, except: [ :index, :new, :create ]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new ( user_params )
+
+    # if @user.save
+    #   redirect_to root_url ( @user )
+    # else
+    #   flash.now[:alert] = @user.errors
+    #   render :new
+  end
+
   def index
     @users = User.all
   end
@@ -8,6 +25,36 @@ class UsersController < ApplicationController
     @user = User.find_by_id(params[:id])
   end
 
+  def edit
+  end
 
+  def update
+    # if
+    #   @user.update_attributes ( user_params )
+    #   redirect_to root_url( @user )
+    # else
+    #   flash.now[:alert] = @user.errors
+    #   render :edit
+    # end
+  end
+
+  def destroy
+    # @user.destroy
+    # redirect_to index_url, notice: "Deleted #{@user.email}."
+  end
+
+
+end
+
+
+  private
+
+  def get_user
+    @user = User.find( params[:id] )
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 
 end
--- app/views/pins/show.json.rabl
+++ app/views/pins/show.json.rabl
@@ -6,3 +6,4 @@ attributes :lat
 attributes :guide_id
 attributes :activity
 attributes :description
+attributes :first_name
