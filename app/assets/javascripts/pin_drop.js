80589218cb85183c36eabf1678cb86666dcde23c
Victor Lin <victor@victorlinphoto.com>
2014-04-22 10:53:26 +0800

    successful tracking of draggable pin

--- app/assets/javascripts/pin_drop.js
+++ app/assets/javascripts/pin_drop.js
@@ -3,13 +3,24 @@ $( "#addpin" ).on( "click", function() {
   function placeMarker(location) {
   var marker = new google.maps.Marker({
       position: location,
+      draggable:true,
+      animation: google.maps.Animation.DROP,
       map: window.map
   });
   map.setCenter(location);
+  google.maps.event.addListener(marker, 'dragend', function (event) {
+    var latitude = this.getPosition().lat();
+    var longitude = this.getPosition().lng();
+    console.log("lat: " + latitude);
+    console.log("long: " + longitude);
+  });
+  // var latitude = location.k;
+  // var longitude = location.A;
+  // console.log("lat: " + latitude);
+  // console.log("long: " + longitude);
   console.log(location);
   }
-
-  google.maps.event.addListener(map, 'click', function(event) {
-    placeMarker(event.latLng);
-  });
-  });
+  // google.maps.event.addListener(map, 'click', function(event) {
+  //   placeMarker(event.latLng);
+  // });
+});
