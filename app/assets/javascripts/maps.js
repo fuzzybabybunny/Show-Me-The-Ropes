c5186a069c091df8b4fd412a75a102aaa22dbce0
Screen Goose <sarrieschaz@hotmail.com>
2014-04-23 14:43:49 +0800

    Added database pins

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,4 +1,48 @@
 // INITIALIZE THE MAP
+function initialize() {
+  var map_canvas = document.getElementById('map_canvas');
+  var myLatlng = new google.maps.LatLng(22.25, 114.1667);
+  var map_options = {
+    center: new google.maps.LatLng(22.25, 114.2),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [
+      ]
+   }
+  var map = new google.maps.Map(map_canvas, map_options);
+  window.map = map;
+
+  // PRE-SET MARKERS
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title:"This is marker 1!",
+    id: 1
+  });
+
+  // GET ALL PINS FROM DB
+  getAllPins()
+
+
+  // CONTENT OF THE MARKERS
+  var pinContent = document.getElementById('pinContent');
+  var infowindow = new google.maps.InfoWindow({
+      content: pinContent,
+    });
+
+
+  // LISTENING FOR ICON CLICKS
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+    console.log(marker.position);
+    console.log("Pin ID: " + marker.id);
+    getPinData(marker.id);
+  });
+
+}
+
+
+// GET DATA FROM PINS
 function getMapData(id, template) {
   $.ajax({
     url: "/api/users/"+id,
@@ -23,109 +67,43 @@ function getPinData(id, template) {
     var pinGuideFirstName = data["pin"]["guide_first_name"];
     var pinGuideLastName = data["pin"]["guide_last_name"];
     var pinGuideRating = data["pin"]["guide_rating"];
-    // var pinGuideReviews = data["pin"]["guide_textReview"];
 
     $('#firstHeading').html(pinTitle);
     $('#bodyContent').html(pinDescription);
     $('#pinGuideName').html(pinGuideFirstName + " " + pinGuideLastName);
     $('#pinGuideRating').html(pinGuideRating);
-    // $('#pinGuideReviews').html(pinGuideReviews);
   })
 }
 
-function getAllPins() {
-  $.ajax({
-    url: "/api/pins/",
-    type: "GET",
-    data: "JSON"
-  }).success(function(data){
-    for (var i = 0; i < data["pins"].length; i++) {
-    var pinTitle = data["pins"][i]["pin"]["id"];
-    var pinLong = data["pins"][i]["pin"]["long"];
-    var pinLat = data["pins"][i]["pin"]["lat"];
-    console.log(pinTitle);
-    console.log(pinLat);
-    console.log(pinLong);
-    }
-  })
-}
-getAllPins()
-
-function initialize() {
-  var map_canvas = document.getElementById('map_canvas');
-  var myLatlng = new google.maps.LatLng(22.25, 114.1667);
-  var map_options = {
-    center: new google.maps.LatLng(26.11, -2.46),
-    zoom: 3,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles: [
-        // {
-        //   stylers: [
-        //     { hue: "#00ffe6" },
-        //     { saturation: -20 }
-        //   ]
-        // },{
-        //   featureType: "road",
-        //   elementType: "geometry",
-        //   stylers: [
-        //     { lightness: 100 },
-        //     { visibility: "simplified" }
-        //   ]
-        // },{
-        //   featureType: "road",
-        //   elementType: "labels",
-        //   stylers: [
-        //     { visibility: "off" }
-        //   ]
-        // }
-      ]
-   }
-  var map = new google.maps.Map(map_canvas, map_options);
-  window.map = map;
-
-// PRE-SET MARKERS
+// DROP DATABASE PINS
+function dbMarker(pinLat, pinLong, pinID){
   var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    title:"This is marker 1!",
-    id: 1
-  });
+      position: new google.maps.LatLng(pinLat, pinLong),
+      map: map,
+      title: "Database pins",
+      id: pinID
+      });
+  }
+
+  function getAllPins() {
+    $.ajax({
+      url: "/api/pins/",
+      type: "GET",
+      data: "JSON"
+    }).success(function(data){
+      for (var i = 0; i < data["pins"].length; i++) {
+        var pinID = data["pins"][i]["pin"]["id"];
+        var pinLong = data["pins"][i]["pin"]["long"];
+        var pinLat = data["pins"][i]["pin"]["lat"];
+        console.log(pinTitle);
+        console.log(pinLat);
+        console.log(pinLong);
+
+        dbMarker(pinLat, pinLong, pinID)
+      }
+    })
+  }
 
 
-// CONTENT OF THE MARKERS
-
-  var pinContent = document.getElementById('pinContent');
-
-  var infowindow = new google.maps.InfoWindow({
-      content: pinContent,
-    });
-// ADDING MARKERS
-
-  // function placeMarker(location) {
-  // var marker = new google.maps.Marker({
-  //     position: location,
-  //     map: map
-  // });
-
-  // map.setCenter(location);
-  // console.log(location);
-  // }
-
-  // google.maps.event.addListener(map, 'click', function(event) {
-  //   placeMarker(event.latLng);
-  // });
-
-
-// LISTENING FOR ICON CLICKS
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-    console.log(marker.position);
-    console.log("Pin ID: " + marker.id);
-    getPinData(marker.id);
-  });
-
-}
-
-// CREATING THE MAP
+// CREATE THE MAP
 google.maps.event.addDomListener(window, 'load', initialize);
-
