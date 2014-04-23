25af07d304ce3bf49386e29a6486684f64d27837
Victor Lin <victor@victorlinphoto.com>
2014-04-23 18:49:40 +0800

    Changed email to be more customized based on variables

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,12 +3,12 @@ class MessageMailer < ActionMailer::Base
   default from: "victor@victorlinphoto.com"
 
   def welcome_email(message_params)
-    @first_name = message_params["firstName"]
-    @last_name = message_params["lastName"]
+    @firstName = message_params["firstName"]
+    @lastName = message_params["lastName"]
     @messageTitle = message_params["messageTitle"]
     @messageBody = message_params["messageBody"]
     @currentUserEmail = message_params["currentUserEmail"]
-    mail(to: "victor@victorlinphoto.com", subject: @messageTitle)
+    mail(to: "victor@victorlinphoto.com", subject: "Show Me the Ropes Message: " + @messageTitle)
   end
 
 end
